39329fccd9eb07c15e2af79cafad5b31e897bfe8
Pooja Yadav <poyadav@ucsd.edu>
2020-01-21 16:05:51 -0800

    Initial Commit

---
+++ tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipster
+//
+//  Created by Pooja Yadav on 1/21/20.
+//  Copyright © 2020 Pooja Yadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
